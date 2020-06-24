39111734fdcc0e9c1bcb307814f5893e3fbe24a1
liangqiding <742740345@qq.com>
2020-06-24 11:20:54 +0800

    add

---
+++ 07.spring-boot-admin/server/src/main/java/admin/ServerApplication.java
@@ -0,0 +1,18 @@
+package admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Administrator
+ */
+@SpringBootApplication
+@EnableAdminServer
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+}
