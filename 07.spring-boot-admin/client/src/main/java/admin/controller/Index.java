93bb864c0118991ec81a49f7fa8f2660064c9072
liangqiding <742740345@qq.com>
2020-06-24 11:14:21 +0800

    add project

---
+++ 07.spring-boot-admin/client/src/main/java/admin/controller/Index.java
@@ -0,0 +1,45 @@
+package admin.controller;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @Author: QiDing
+ * @DateTime: 2020/6/24 0024 8:54
+ * @Description: TODO
+ */
+@RestController
+public class Index {
+   private final static Logger logger= LoggerFactory.getLogger(Index.class);
+    @GetMapping("/get")
+    public String getMsg() {
+        logger.info("getMsg success");
+        return "getMsg success";
+    }
+
+    @GetMapping("/test")
+    public String error() {
+        logger.error("error 模拟");
+        return "test error ";
+    }
+
+    @PostMapping("/post")
+    public String postMsg() {
+        logger.info("postMsg success");
+        return "postMsg success";
+    }
+
+    @DeleteMapping("/delete")
+    public String deleteMsg() {
+        logger.info("deleteMsg success");
+        return "deleteMsg success";
+    }
+
+    @PutMapping
+    public String PutMsg() {
+        logger.info("PutMsg success");
+        return "PutMsg success";
+    }
+}
